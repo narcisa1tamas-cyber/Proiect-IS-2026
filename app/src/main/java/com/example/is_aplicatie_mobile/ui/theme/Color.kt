9b700c3f255aa1edb5ccf295c38745dedd17be88
narcisa1tamas-cyber <narcisa1.tamas@gmail.com>
2026-04-21 23:49:50 +0300

    Added IS-Aplicatie-Mobile as a submodule

---
+++ app/src/main/java/com/example/is_aplicatie_mobile/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.is_aplicatie_mobile.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val MedicalBlue = Color(0xFF0077B6)
+val MedicalLightBlue = Color(0xFFADE8F4)
+val MedicalGreen = Color(0xFF90BE6D)
+val PureWhite = Color(0xFFFFFFFF)
+val LightGray = Color(0xFFF1F1F1)
